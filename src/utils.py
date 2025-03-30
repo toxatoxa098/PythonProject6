760a79fce15e6d35035cc2f498cbba0a9c0c82d1
Anton Masloynok <antoxa.toxa.83@mail.ru>
2025-03-30 19:09:50 +0300

    В модуле main.py созданы два класса: Product и Category, также в модуле utils написана функция, которая загружает данные из json-файла

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+from src.main import Category, Product
+
+
+def read_json(path: str) -> dict:
+    full_path = os.path.abspath(path)
+    with open(full_path, "r", encoding="UTF=8") as file:
+        data = json.load(file)
+    return data
+
+
+def create_class_objects_from_json(data):
+    class_objects = []
+    for object in data:
+        products = []
+        for product in object["products"]:
+            products.append(Product(**product))
+        object["products"] = products
+        class_objects.append(Category(**object))
+
+    return class_objects
+
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(current_dir, "..", "data")
+    json_file_path = os.path.join(data_dir, "product.json")
+    gadgets = read_json(json_file_path)
+    object_gadgets = create_class_objects_from_json(gadgets)
+    print(object_gadgets)
+
